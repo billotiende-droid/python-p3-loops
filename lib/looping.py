1c129fd0d2658c2d1e123f4ab74a262ed1e7851b
billotiende-droid <bill.otiende@student.moringaschool.com>
2025-11-13 01:53:25 +0300

    squared-elements

--- lib/looping.py
+++ lib/looping.py
@@ -1,23 +1,37 @@
 #!/usr/bin/env python3
 
-def happy_new_year():
-    counter = 10
-    while counter >= 1:
-        print(counter)
-        counter -= 1
+# def happy_new_year():
+#     counter = 10
+#     while counter >= 1:
+#         print(counter)
+#         counter -= 1
            
-    print("Happy New Year!")    
+#     print("Happy New Year!")    
          
-    # code goes here!
+#     # code goes here!
 
-    pass
+#     pass
+
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    squared_list = []
+
+    for num in int_list:
+        squared_list.append(num * num)
+
+    return squared_list 
+
+print(square_integers([1, 2, 3, 4, 5]))   
+    
+
+
+
+    
+
+ # code goes here!
 
 def fizzbuzz():
     # code goes here!
     pass
 
-print(happy_new_year())
+# print(happy_new_year())
