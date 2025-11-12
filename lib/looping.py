6219c0a3c96295213a7db73c912ab38aa513bb02
billotiende-droid <bill.otiende@student.moringaschool.com>
2025-11-13 02:14:08 +0300

    fizz-divisbility-switch-case

--- lib/looping.py
+++ lib/looping.py
@@ -1,16 +1,16 @@
-#!/usr/bin/env python3
+# !/usr/bin/env python3
 
-# def happy_new_year():
-#     counter = 10
-#     while counter >= 1:
-#         print(counter)
-#         counter -= 1
+def happy_new_year():
+    counter = 10
+    while counter >= 1:
+        print(counter)
+        counter -= 1
            
-#     print("Happy New Year!")    
+    print("Happy New Year!")    
          
-#     # code goes here!
+    # code goes here!
 
-#     pass
+    pass
 
 
 def square_integers(int_list):
@@ -23,15 +23,22 @@ def square_integers(int_list):
 
 print(square_integers([1, 2, 3, 4, 5]))   
     
-
-
-
-    
-
  # code goes here!
 
 def fizzbuzz():
+    for i in range (1,101):
+        if i % 3 == 0 and i % 5 == 0:   # check if divisible by 3 and 5
+            print("FizzBuzz")
+        elif i % 3 == 0 :       # check if divisible by 3
+            print("Fizz") 
+        elif i % 5 == 0 :       # check if divisible by 5
+            print ("Buzz")
+        else:
+            print(i)       
+
+fizzbuzz()
+
     # code goes here!
-    pass
+pass
 
 # print(happy_new_year())
