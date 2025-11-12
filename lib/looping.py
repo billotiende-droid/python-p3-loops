924966bb79bdec72554812fc15f2fc6bf1e40067
billotiende-droid <bill.otiende@student.moringaschool.com>
2025-11-12 21:23:05 +0300

    happy-year-while-loop

--- lib/looping.py
+++ lib/looping.py
@@ -1,7 +1,15 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
+    counter = 10
+    while counter >= 1:
+        print(counter)
+        counter -= 1
+           
+    print("Happy New Year!")    
+         
     # code goes here!
+
     pass
 
 def square_integers(int_list):
@@ -11,3 +19,5 @@ def square_integers(int_list):
 def fizzbuzz():
     # code goes here!
     pass
+
+print(happy_new_year())
